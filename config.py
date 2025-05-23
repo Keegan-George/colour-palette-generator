670bc5c188169d6f9c46d7434e7e9e76724f3ea0
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-05-23 10:04:38 -0400

    Add doc string to functions. Add variable to store top colours.

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "jfif"}
+NUMBER_OF_COLOURS = 10
--- utils.py
+++ utils.py
@@ -1,29 +1,38 @@
 from PIL import Image
-from config import ALLOWED_EXTENSIONS
+from config import *
 
 
 def allowed_file(filename: str) -> bool:
+    """
+    Return True if the filename is allowed. False otherwise.
+    """
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 def most_frequent_colours(
     img: Image.Image,
 ) -> list[tuple[int, tuple[int, int, int], str]]:
-    # determine maximum possible colours of the image
-    max_colours = img.size[0] * img.size[1]
+    """
+    Return a list of the most common colours in an image.
+    Each list item contains the colour frequency, RGB values, HEX value, and percentage in that order.
+    """
 
-    # get all colours in image and sort descending
-    all_colours = img.getcolors(maxcolors=max_colours)
+    # number of pixels in the image
+    width, height = img.size
+    num_pixels = width * height
+
+    # get all colours in image and sort in descending order of frequency
+    all_colours = img.getcolors(maxcolors=num_pixels)
     all_colours.sort(reverse=True)
 
-    # keep only top 10 colours
-    frequent_colours = all_colours[:10]
+    # keep only the most frequent colours
+    frequent_colours = all_colours[:NUMBER_OF_COLOURS]
 
     # add hex code and percentage to frequent colours list
     for i in range(len(frequent_colours)):
         r, g, b, *a = frequent_colours[i][1]
         hex = f"#{r:02x}{g:02x}{b:02x}"
-        percentage = f"{frequent_colours[i][0] / max_colours:.2%}"
+        percentage = f"{frequent_colours[i][0] / num_pixels:.2%}"
         frequent_colours[i] = frequent_colours[i] + (hex, percentage)
 
     return frequent_colours
