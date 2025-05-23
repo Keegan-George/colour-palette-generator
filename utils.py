8300d160a618a83a563f849ef752ec55b9996453
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-05-23 10:45:51 -0400

    Use dict to store colour palette values

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
-from PIL import Image
 from config import *
+from PIL import Image
 
 
 def allowed_file(filename: str) -> bool:
@@ -21,18 +21,27 @@ def most_frequent_colours(
     width, height = img.size
     num_pixels = width * height
 
-    # get all colours in image and sort in descending order of frequency
-    all_colours = img.getcolors(maxcolors=num_pixels)
-    all_colours.sort(reverse=True)
+    # get all colours in image and sort in descending order of pixel count
+    colours = img.getcolors(maxcolors=num_pixels)
+    colours.sort(reverse=True)
 
-    # keep only the most frequent colours
-    frequent_colours = all_colours[:NUMBER_OF_COLOURS]
+    # keep only the top most frequent colours
+    frequent_colours = colours[:NUMBER_OF_COLOURS]
 
-    # add hex code and percentage to frequent colours list
-    for i in range(len(frequent_colours)):
-        r, g, b, *a = frequent_colours[i][1]
+    # create colour palette
+    colour_palette = []
+    for colour in frequent_colours:
+        r, g, b, *a = colour[1]
         hex = f"#{r:02x}{g:02x}{b:02x}"
-        percentage = f"{frequent_colours[i][0] / num_pixels:.2%}"
-        frequent_colours[i] = frequent_colours[i] + (hex, percentage)
+        percentage = f"{colour[0] / num_pixels:.2%}"
+
+        colour_palette.append(
+            {
+                "count": colour[0],
+                "rgb": colour[1],
+                "hex": hex,
+                "percentage": percentage,
+            }
+        )
 
     return frequent_colours
